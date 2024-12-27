41198fcc68cb3e562a4ca06b4090ecbc11922a08
Burhan Chughtai <burhanchughtai744@gmail.com>
2024-12-27 19:01:55 +0500

    'updated-code'

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -18,24 +18,24 @@ const Page = () => {
         <div className="relative container w-full z-20 flex flex-col text-center sm:text-left justify-center md:justify-start md:left-0 xl:left-8 md:left-8">
           {/* Heading */}
           <h1 className="text-2xl sm:text-3xl font-bold tracking-wider leading-relaxed text-white xl:mb-5 md:mb-4">
-            <span className="block xl:text-5xl md:text-3xl tracking-wide">
+            <span className="block xl:text-5xl md:text-3xl tracking-wide text-xl  ">
               The UK&apos;s Trusted
             </span>
-            <span className="block mt-2 xl:text-5xl md:text-3xl tracking-wide">
+            <span className="block text-xl xl:mt-2 xl:text-5xl md:text-3xl tracking-wide">
               Online Pharmacy
             </span>
-            <span className="mt-2 xl:text-5xl md:text-3xl tracking-wide">
+            <span className="mt-2 text-xl xl:text-5xl md:text-3xl tracking-wide">
               Since 2015
             </span>
           </h1>
           <div className="justify-start lg:w-[40dvw]  pt-2 mb-2 sm:mb-0 block">
-            <span className="text-white">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. A
-              nesciunt totam esse accusantium vel nulla harum sapiente ipsam
-              nihil quaerat? Sunt voluptatem blanditiis doloremque facilis est
-              ullam in repellat mollitia? Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Ea accusantium nulla laudantium explicabo
-              ratione pariatur temporibus minus dolorem ad doloribus.
+            <span className="text-white text-xs sm:text-base px-1 xl:px-0">
+              Pharmica offers a wide range of treatments through our free online
+              prescription service. Our ethos is that by providing cutting-edge
+              technology to automate and innovate we can deliver a safer, more
+              affordable health care experience. Our service is safe, discreet
+              and convenient. All medicines are dispensed from our Central
+              London-based registered pharmacy.
             </span>
             {/* Button or Call to Action */}
             <div className="xl:mt-5 sm:mt-6 md:mt-4">
@@ -49,90 +49,84 @@ const Page = () => {
 
       {/* Main Content Section */}
       <div className="container mx-auto mb-[70px] ">
-      <div className="grid  w-full h-auto gap-2 p-0 relative">
+        <div className="grid  w-full h-auto gap-2 p-0 relative">
           {/* Other Sections */}
-          <div className="w-full   my-8">
-            <h1 className="block ml-0 justify-start lg:text-[48px] font-extrabold">
+          <div className="w-full my-8">
+            <h1 className="block ml-2 xl:ml-0 justify-start text-xl  lg:text-[48px] font-extrabold">
               Company&nbsp;
               <span className="relative text-hoverUnderlineColor underline-background tracking-widest">
                 Information
               </span>
             </h1>
-            <div className="grid lg:grid-cols-[30%_70%] grid-rows-1 gap-y-1 h-72">
-              <div className="  mt-7 ">
-                <p className="tracking-wide">
-                  <span className="block">
-                    This website is operated by Pharmica Ltd. Pharmica Ltd is
-                    registered
-                  </span>
-                  <span className="block">
-                    in England and Wales under company number
-                    <span className="text-hoverUnderlineColor">9668055</span>
-                  </span>
-                  <span className="block">
-                    DUNS Number:{" "}
-                    <span className="text-hoverUnderlineColor">
-                      22-097-8327
-                    </span>
-                  </span>
-                  VAT Number:{" "}
-                  <span className="text-hoverUnderlineColor">
-                    GB 217 7259 95
-                  </span>
-                </p>
-                <h1 className="block lg:text-xl font-bold my-2">
-                  Registered Office:
-                </h1>
-                <p className="tracking-wide w-[276px] ">
-                  <span className="block">
-                    Registered Office: 236 Gray&apos;s Inn Rd London WC1X 8HB
-                    United
-                  </span>
-                  <span className="block">
-                    Kingdom 020 7112 9014
-                    <span className="block text-hoverUnderlineColor font-semibold">
-                      help@pharmica.co.uk
-                    </span>
-                  </span>
-                </p>
-              </div>
+            <div className="grid grid-rows-2 sm:grid-cols-1 lg:grid-cols-[30%_70%] lg:grid-rows-1 gap-y-4 sm:gap-y-2 h-auto lg:h-72">
+  {/* Text Section */}
+  <div className="mt-7 order-2 lg:order-1">
+    <p className="tracking-wide">
+      <span className="block">
+        This website is operated by Pharmica Ltd. Pharmica Ltd is registered
+      </span>
+      <span className="block">
+        in England and Wales under company number{" "}
+        <span className="text-hoverUnderlineColor">9668055</span>
+      </span>
+      <span className="block">
+        DUNS Number:{" "}
+        <span className="text-hoverUnderlineColor">22-097-8327</span>
+      </span>
+      VAT Number:{" "}
+      <span className="text-hoverUnderlineColor">GB 217 7259 95</span>
+    </p>
+    <h1 className="block lg:text-xl font-bold my-2">Registered Office:</h1>
+    <p className="tracking-wide w-full sm:w-[276px]">
+      <span className="block">
+        Registered Office: 236 Gray&apos;s Inn Rd London WC1X 8HB United
+      </span>
+      <span className="block">
+        Kingdom 020 7112 9014
+        <span className="block text-hoverUnderlineColor font-semibold">
+          help@pharmica.co.uk
+        </span>
+      </span>
+    </p>
+  </div>
 
-              {/* Images */}
-              <div className=" mt-7 w-full relative">
-                <Image
-                  src="/bg3.png"
-                  width={400}
-                  height={60}
-                  className="w-full bg-cover h-[260px]  "
-                  alt="Building"
-                />
-                <div className="absolute inset-0 bg-black opacity-50"></div>
-              </div>
-            </div>
+  {/* Image Section */}
+  <div className="mt-7 w-full relative order-1 lg:order-2">
+    <Image
+      src="/bg3.png"
+      width={400}
+      height={60}
+      className="w-full bg-cover h-[260px] sm:h-[300px] lg:h-[260px]"
+      alt="Building"
+    />
+    <div className="absolute inset-0 bg-black opacity-50"></div>
+  </div>
+</div>
+
+
           </div>
         </div>
       </div>
 
       <div className="bg-gray-100">
-  <div className="w-full grid container mx-auto lg:grid-cols-2 grid-rows-1 gap-y-0  md:h-72 lg:h-auto">
-    <div className="my-16">
-      <Image
-        src="/customerReview.png"
-        width={400}
-        height={60}
-        alt="Image"
-        className="ml-0 bg-cover block"
-      />
-    </div>
-    <div className="grid grid-rows-5 gap-y-0  md:ml-10 my-16">
-      <div>
-        <h1 className="lg:text-5xl font-extrabold">
-          Customer&nbsp;
-          <span className="relative text-black redunderline-background z-50 tracking-widest">
-            Reviews
-          </span>
-        </h1>
-
+        <div className="w-full grid container mx-auto lg:grid-cols-2 grid-rows-1 gap-y-0  md:h-72 lg:h-auto">
+          <div className="my-16">
+            <Image
+              src="/customerReview.png"
+              width={400}
+              height={60}
+              alt="Image"
+              className="ml-0 bg-cover block"
+            />
+          </div>
+          <div className="grid grid-rows-5 gap-y-0  md:ml-10 my-16">
+            <div>
+              <h1 className="lg:text-5xl font-extrabold">
+                Customer&nbsp;
+                <span className="relative text-black redunderline-background z-50 tracking-widest">
+                  Reviews
+                </span>
+              </h1>
             </div>
             <div className="flex justify-start items-center">
               {[...Array(5)].map((_, index) => (
@@ -152,15 +146,12 @@ const Page = () => {
               </span>
             </div>
             <div>
-              <span className="lg:text-base " style={{lineHeight:'25px'}}>
+              <span className="lg:text-base " style={{ lineHeight: "25px" }}>
                 <span className="block lg:w-[504px]  ">
                   Our customers trust us to deliver top-quality service.
                 </span>
-                Read more reviews on Google and 
-                
-                <span className="text-green-500">
-                TrustPilot!{" "}
-                </span>
+                Read more reviews on Google and
+                <span className="text-green-500">TrustPilot! </span>
               </span>
             </div>
 
@@ -173,65 +164,68 @@ const Page = () => {
         </div>
       </div>
 
-      <div className=""
-      style={{
-        backgroundImage: "url('/bg.png')",
-        backgroundRepeat:'no-repeat',
-        backgroundPosition:'center',
-        backgroundSize:"cover"
-      }}
-      > 
+      <div
+        className=""
+        style={{
+          backgroundImage: "url('/bg.png')",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
         <div className="w-full grid container mx-auto lg:grid-cols-2 grid-rows-1   md:h-72 lg:h-full ">
           {/* First Section */}
           <div className="  bg- text-white py-14">
             <h1 className="lg:text-[36px] font-extrabold  leading-relaxed pb-5 ">
               Pharmacy&nbsp;
               <span className="text-white redunderline-background z-50 tracking-widest">
-              Regulation
+                Regulation
               </span>
-            </h1> 
-              <div className=" left-0 block tracking-wide space-y-1">
-                <p
-                  className="max-w-[504px] "
-                  style={{
-                    lineHeight: "44.6px",
-                    fontSize: "20px",
-                    fontWeight: "700",
-                  }}
-                >
-                  The pharmacy is registered with the General Pharmaceutical
-                  Council with number 9012030. The superintendent pharmacist for
-                  Pharmica is: Ana Carolina Osorio De Faria Goncalves GPhC
-                  Number: 2088658
-                </p>
-                <span className="block text-[20px]">
-                  <li className="custom-list-item text-white">
-                    Dr Munawar Iqbal &nbsp;
-                    <span className="text-hoverUnderlineColor">
-
-                     (GPhC Number 2059871)
-                    </span>
-                  </li>
-                  <li className="custom-list-item text-white">
-
-                    Dr Munawar Iqbal&nbsp;
-                    <span className="text-hoverUnderlineColor">
-                     (GPhC Number 2059871)
-                    </span>
-                  </li>
-                </span>
-              </div> 
+            </h1>
+            <div className=" left-0 block tracking-wide space-y-1">
+              <p
+                className="max-w-[504px] "
+                style={{
+                  lineHeight: "44.6px",
+                  fontSize: "20px",
+                  fontWeight: "700",
+                }}
+              >
+                The pharmacy is registered with the General Pharmaceutical
+                Council with number 9012030. The superintendent pharmacist for
+                Pharmica is: Ana Carolina Osorio De Faria Goncalves GPhC Number:
+                2088658
+              </p>
+              <span className="block text-[20px]">
+                <li className="custom-list-item text-white">
+                  Dr Munawar Iqbal &nbsp;
+                  <span className="text-hoverUnderlineColor">
+                    (GPhC Number 2059871)
+                  </span>
+                </li>
+                <li className="custom-list-item text-white">
+                  Dr Munawar Iqbal&nbsp;
+                  <span className="text-hoverUnderlineColor">
+                    (GPhC Number 2059871)
+                  </span>
+                </li>
+              </span>
+            </div>
           </div>
           {/* Second Section */}
           <div className="grid grid-rows-3  md:ml-10 text-white w-[504px] justify-start items-center mt-[63px] h-[356px] ml-2 ">
             <div className="block">
-              <span  className="max-w-[504px] "
-                  style={{
-                    lineHeight: "44.6px",
-                    fontSize: "20px",
-                    fontWeight: "700",
-                  }}>
-              For more information or to view registration details visit the General Pharmaceutical Council&nbsp;s website.Responsible Pharmacist:
+              <span
+                className="max-w-[504px] "
+                style={{
+                  lineHeight: "44.6px",
+                  fontSize: "20px",
+                  fontWeight: "700",
+                }}
+              >
+                For more information or to view registration details visit the
+                General Pharmaceutical Council&nbsp;s website.Responsible
+                Pharmacist:
               </span>
             </div>
             <div className="flex justify-start items-start  mt-14">
@@ -243,13 +237,10 @@ const Page = () => {
                 className="block w-[166px] h-[66px]  "
               />
             </div>
-            <div className=" "  >
-              <span className="text-[20px] w-10"> 
-              <span className="block">
-              Rehma Gill&nbsp; 
-
-              </span>
-              (GPhC number 2225869)
+            <div className=" ">
+              <span className="text-[20px] w-10">
+                <span className="block">Rehma Gill&nbsp;</span>
+                (GPhC number 2225869)
               </span>
             </div>
           </div>
@@ -257,13 +248,19 @@ const Page = () => {
       </div>
       <div className="w-ful">
         <div>
-          <Image src='/bg1.png' height={60} width={100} alt="" className="w-40 absolute" />
+          <Image
+            src="/bg1.png"
+            height={60}
+            width={100}
+            alt=""
+            className="w-40 absolute"
+          />
         </div>
         <HowDoesItWorks />
       </div>
       <div className="w-full border border-black ">
-        <QualifiedTeamAboutPage/>
-        </div>   
+        <QualifiedTeamAboutPage />
+      </div>
     </div>
   );
 };
